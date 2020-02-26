7c23cc612d305798d77ea08bc7b2e26229e8d924
Webbuildermn <gerald.ryan40@yahoo.com>
2020-02-25 18:44:34 -0600

    Passed Cap Names map function toUpper.

--- index.js
+++ index.js
@@ -239,8 +239,12 @@ function getFullNames(runners) { //Works but not in the browser
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const fNames = runners.map(function(element, i){
+    return element.first_name.toUpperCase() 
+  })
+console.log(fNames)
+return (fNames)
 }
 
 /**
